e81efc50993a6b237e3b196f55202163e21cf32a
Robert Stebbins <48532176+robertstebbins@users.noreply.github.com>
2022-07-31 22:52:20 -0500

    Revert "update outstandingRequest message"
    This reverts commit da6ce966b211d05431d186f9ed5cfaa903debe2b.

--- Library.java
+++ Library.java
@@ -835,7 +835,7 @@ public class Library
                         int successValue = system.checkOutItem(Integer.parseInt(itemID), currentUserID);
                         if(successValue == 1)
                         {
-                            JOptionPane.showMessageDialog(checkoutFrame, "Outstanding request");
+                            JOptionPane.showMessageDialog(checkoutFrame, "Critical Error");
                         }
                         else if(successValue == 2)
                         {
